1441c25ab3a1e0efe4dd691f5287e7aae40a94a6
unknown <tiphaine.ponsonnet@laplateforme.io>
2024-12-05 16:19:52 +0100

    commencement du footer

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -5,7 +5,15 @@ function Footer(){
     return(
         <footer>
             <img className="socials" src={socials} alt="Résaux Sociaux"/>
-
+            <div className="footer-links">
+                <ul>
+                    <li><a href="#about">À propos</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                    <li><a href="#help">Aide</a></li>
+                    <li><a href="#terms">Conditions d'utilisation</a></li>
+                    <li><a href="#privacy">Politique de confidentialité</a></li>
+                </ul>
+            </div>
         </footer>
     );
 };
